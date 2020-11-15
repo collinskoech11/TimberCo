00d777e972a2183b4d2ec5e07265fb1d368d01fc
Collins Koech <collinskoechck34@gmail.com>
2020-11-16 02:46:15 +0300

    added dimensions array

--- main.java
+++ main.java
@@ -6,13 +6,16 @@ public class main{
       Scanner s = new Scanner(System.in);
       int length = 9;
       int [] categoryName = new int[length];
-      s.close();
+      
       System.out.println("Enter the Category Titles:");
 
       for(int i=0; i<length; i++ ) {
          categoryName[i] = s.nextInt();
       }
-
+      int [] categoryDimension = new int[length];
+    
+      System.out.println("Enter Category dimensions")
+      s.close();
       System.out.println(Arrays.toString(categoryName));
    }
 }
