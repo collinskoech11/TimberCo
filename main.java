441a08dfa314f5a4126846ca962e65dc862d8da0
Collins Koech <collinskoechck34@gmail.com>
2020-11-16 03:23:28 +0300

    closing update

--- main.java
+++ main.java
@@ -1,31 +1,33 @@
-import java.util.Arrays;
+
 import java.util.Scanner;
 
 public class main{
    public static void main(String args[]) {
-      Scanner s = new Scanner(System.in);
+      
       int length = 9;
       int [] categoryName = new int[length];
-      
-      System.out.println("Enter the Category Titles:");
-
-      for(int i=0; i<length; i++ ) {
-         categoryName[i] = s.nextInt();
-      }
       int [] categoryDimension = new int[length];
-    
-      System.out.println("Enter Category dimensions:");
+      
 
-      for(int i=0; i<length; i++ ) {
-         categoryDimension[i] = s.nextInt();
+      for(int i=0; i<9; i++ ) {
+         System.out.println("Enter the Category Title:");
+         Scanner s = new Scanner(System.in);
+         categoryName[i] = s.nextInt();
+         s.close();
+      }
+      for(int i=0; i<9; i++ ) {
+         Scanner N = new Scanner(System.in);
+         System.out.println("Enter Category dimension for"+categoryName[i]);
+         categoryDimension[i] = N.nextInt();
+         N.close();
       }
 
-      System.out.println("Category    Dimension");
-      for(int f=0; f<length; f++){
-         System.out.println(categoryName[f]+"  "+categoryDimension[f]);
+      System.out.println("Category:Dimension");
+      for(int f=0; f<9; f++){
+         System.out.println(categoryName[f]+"         "+categoryDimension[f]);
       }
      
-      s.close();
+      
       
    }
 }
