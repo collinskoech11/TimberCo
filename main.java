847e8f50f3b6cb04af45eb1b07254b7902568635
Collins Koech <collinskoechck34@gmail.com>
2020-11-17 13:39:37 +0300

    update

--- main.java
+++ main.java
@@ -1,33 +1,24 @@
-
-import java.util.Scanner;
-
-public class main{
-   public static void main(String args[]) {
-      
-      int length = 9;
-      int [] categoryName = new int[length];
-      int [] categoryDimension = new int[length];
-      
-
-      for(int i=0; i<9; i++ ) {
-         System.out.println("Enter the Category Title:");
-         Scanner s = new Scanner(System.in);
-         categoryName[i] = s.nextInt();
-         s.close();
-      }
-      for(int i=0; i<9; i++ ) {
-         Scanner N = new Scanner(System.in);
-         System.out.println("Enter Category dimension for"+categoryName[i]);
-         categoryDimension[i] = N.nextInt();
-         N.close();
-      }
-
-      System.out.println("Category:Dimension");
-      for(int f=0; f<9; f++){
-         System.out.println(categoryName[f]+"         "+categoryDimension[f]);
-      }
-     
-      
-      
-   }
+import java.util.Arrays; 
+public class main {
+public static void main(String[] args){   
+    
+    int[] my_array1 = {
+            1789, 2035, 1899, 1456, 2013, 
+            1458, 2458, 1254, 1472, 2365, 
+            1456, 2165, 1457, 2456
+         };
+            
+    String[] my_array2 = {
+            "Java",
+			
+            "Python",
+            "PHP",
+            "C#",
+            "C Programming",
+            "C++"
+        };        
+    System.out.println("Original numeric array : "+Arrays.toString(my_array1));
+    
+    System.out.println("Original string array : "+Arrays.toString(my_array2));
+    }
 }
