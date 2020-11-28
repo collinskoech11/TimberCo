d901f22383baaf2af7aa34d43d2fad96daec55ee
Collins Koech <35452377+collinskoech11@users.noreply.github.com>
2020-11-28 14:54:37 +0300

    Update main.java

--- main.java
+++ main.java
@@ -9,13 +9,16 @@ public static void main(String[] args){
          };
             
     String[] my_array2 = {
-            "Java",
-			
-            "Python",
-            "PHP",
-            "C#",
-            "C Programming",
-            "C++"
+           "A-1X1",
+	   "B-1X2",
+	   "C-2X2",
+	   "D-2X3",
+	   "E-3X3",
+	   "F-3X4",
+	   "G-4X4",
+	   "H-1X6",
+	   "I-2X6",
+	   "J-3X6"
         };        
     System.out.println("Original numeric array : "+Arrays.toString(my_array1));
     
